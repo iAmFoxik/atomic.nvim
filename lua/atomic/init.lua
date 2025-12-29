95b64a3470de78fa05ec1943afd0a5c620e52193
iAmFoxik <grisha.bespyatyy@gmail.com>
2025-12-29 16:36:34 +0300

    Added: ui

--- lua/atomic/init.lua
+++ lua/atomic/init.lua
@@ -1,9 +1,20 @@
 local M = {}
 
-M.config = { style = "light" }
+M.config = {
+  style = "light"
+  ui = {
+    enabled = true,
+    sep = "│",
+  }
+}
 
-function M.setup()
+function M.setup(opts)
   require("atomic.theme").setup()
+
+  M.opts = vim.tbl_extend("force", M.opts, config or {})
+  if M.opts.ui.enabled then
+    require("atomic.ui").setup(M.opts.ui)
+  end
 end
 
 return M
---
+++ lua/atomic/ui.lua
@@ -0,0 +1,47 @@
+local M = {}
+
+M.width = 1
+
+function M.update_width()
+  local last = vim.api.nvim_buf_line_count(0)
+  M.width = tostring(last):len()
+end
+
+function M.get_lnum()
+  if vim.wo.relativenumber and vim.v.relnum ~= 0 then
+    return vim.v.relnum
+  end
+
+  return vim.v.lnum
+end
+
+function M.render()
+  local sign = vim.v.sign or " "
+
+  if vim.v.virtnum < 0 then
+    return sign .. " " .. string.rep(" ", M.width) .. M.sep
+  end
+
+  local lnum = M.get_lnum()
+
+  return string.format("%s %*d%s", sign, M.width, lnum, M.sep)
+end
+
+function M.setup(opts)
+  M.sep = opts.sep
+
+  vim.api.nvim_create_autocmd(
+    {"BufEnter", "TextChanged", "TextChangerI"},
+    {
+      callback = function()
+        M.update_width()
+      end,
+    }
+  )
+
+  M.update_width()
+
+  vim.opt.statuscolumn = "%!v:lua.require('atomic.ui').render()"
+end
+
+return M
